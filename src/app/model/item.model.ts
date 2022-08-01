b4c8f3e781582a1ec986379d73060b6d20cc0374
James Powell <jlp2dcz@virginia.edu>
2022-08-01 16:57:54 -0400

    added models for item and itemcombo

--- src/app/model/item.model.ts
+++ src/app/model/item.model.ts
@@ -1,3 +1,9 @@
+import { Vendor } from "./vendor.model";
+
 export class Item {
-    
+    itemId: number;
+    vendor: Vendor;
+    name:string;
+    price:number;
+    quantity:number;
 }
--- src/app/model/itemCombo.model.ts
+++ src/app/model/itemCombo.model.ts
@@ -1,3 +1,9 @@
+import { Item } from "./item.model";
+import { Combo } from "./combo.model";
+
 export class ItemCombo {
+    itemComboId: number;
+    combo:Combo;
+    item:Item;
     
 }
