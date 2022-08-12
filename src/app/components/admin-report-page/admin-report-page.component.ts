0502db3aedcd9e9f9d3474a6917bbb2e149d7e34
Kev-Index <kliu0862@bths.edu>
2022-08-12 03:26:49 -0400

    Adjusted report design to incorporate report title

--- src/app/components/admin-report-page/admin-report-page.component.ts
+++ src/app/components/admin-report-page/admin-report-page.component.ts
@@ -55,11 +55,9 @@ export class AdminReportPageComponent implements OnInit {
   public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
     responsive: false
   };
-  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartLabels: string[] = [ 'Hot Dog', 'Pizza', 'Pie' ];
   public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
-    { data: [ 350, 450, 100 ], label: 'Series A' },
-    { data: [ 50, 150, 120 ], label: 'Series B' },
-    { data: [ 250, 130, 70 ], label: 'Series C' }
+    { data: [ 350, 450, 100 ], label: 'Most Popular Items' },
   ];
 
   // Orders Completed Per Day | Bar Chart
@@ -69,8 +67,7 @@ export class AdminReportPageComponent implements OnInit {
   public barChartData: ChartConfiguration<'bar'>['data'] = {
     labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Orders Completed Per Day' },
     ]
   };
   public barChartLegend = true;
