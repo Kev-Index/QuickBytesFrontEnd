966901145942616053446ecfce9d1c10429548bf
sshjerry <defineantoine@gmail.com>
2022-08-16 23:42:22 -0700

    Fixed Customer Menu Page Layout, Progress on posting Request.

--- src/app/components/customer-dashboard-shoppingcart-productlist/customer-dashboard-shoppingcart-productlist.component.ts
+++ src/app/components/customer-dashboard-shoppingcart-productlist/customer-dashboard-shoppingcart-productlist.component.ts
@@ -22,39 +22,47 @@ export class CustomerDashboardShoppingcartProductlistComponent implements OnInit
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import { Item } from 'src/app/model/item.model';
 import { ItemService } from 'src/app/service/item.service';
+import { RequestService } from 'src/app/service/request.service';
+import { CustomerService } from 'src/app/service/customer.service';
+import { Request } from 'src/app/model/request.model';
 @Component({
   selector: 'app-customer-dashboard-shoppingcart-productlist',
   templateUrl: './customer-dashboard-shoppingcart-productlist.component.html',
   styleUrls: ['./customer-dashboard-shoppingcart-productlist.component.css']
 })
 
-
-/*
-export class CustomerDashboardShoppingcartProductlistComponent {
-  dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['name', 'price'];
-  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement: CultureFoodsElement | null;
-}
-*/
 export class CustomerDashboardShoppingcartProductlistComponent {
   columnsToDisplay = ['name', 'price'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement: Item | null;
 
   dataSource: MatTableDataSource<Item>
+  item:Item;
   items: Item[];
   vendorId: string;
+
+  // START OF ADD ITEM
+  request:Request;
+  // END OF ADD ITEM
   
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private itemsService:ItemService) {
+  constructor(private itemsService:ItemService, private requestService:RequestService, private customerService:CustomerService) {
     this.vendorId = localStorage.getItem('vendorId');
     // Assign the data to the data source for the table to render
   }
   ngOnInit(): void {
+    // START OF ADD ITEM
+    this.requestService.fetchRequests().subscribe({
+      next: (data) => {
+        //console.log(data);
+      }
+      });
+    // END OF ADD ITEM
+
+
     this.itemsService.getItemsByVendorId(this.vendorId).subscribe({
       next:(data)=>{
         this.items=data;
@@ -63,7 +71,6 @@ export class CustomerDashboardShoppingcartProductlistComponent {
         this.dataSource.sort = this.sort;
       }
     });
-    location.reload();
   }
 
   applyFilter(event: Event) {
@@ -73,5 +80,58 @@ export class CustomerDashboardShoppingcartProductlistComponent {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  addItem(item:Item){
+    //console.log(item);
+    //console.log(item.itemId);
+    let request: Request={
+      totalPrice:0,
+      status:"0",
+      orderTime:"2022-08-14",
+      endTime:"2022-08-15"
+    };
+
+    this.customerService.getCustomerById().subscribe({
+      next:(data)=>{
+        let customerId = data.customerId;
+        this.requestService.fetchRequestByCustomerId(customerId).subscribe({
+          next:(data)=>{
+            
+            // CHECK IF REQUEST EXISTS - If you login to kevin, requsts are shown
+            // Use If statement?
+
+            // If no request exists - console.log(data); returns []
+            console.log("CUSTOMER ID: " + localStorage.getItem('roleId'));
+            console.log("VENDOR ID: " + item.vendor.vendorId);
+            console.log("REQUEST:" +request +
+            "\nVENDOR ID: " + item.vendor.vendorId,
+            "\nCUSTOMER ID: " + localStorage.getItem('roleId'));
+
+
+            /**
+             * this.requestService.postRequest(request,item.vendor.vendorId,parseInt(localStorage.getItem('roleId'))).subscribe({
+              next:(data)=>{
+                console.log(data);
+              }
+            });
+             */
+            this.requestService.postRequest(request,parseInt(localStorage.getItem('roleId')),item.vendor.vendorId).subscribe({
+              next:(data)=>{
+                console.log(data);
+                this.request = data;
+                console.log("Current Request: " + this.request);
+                let currentRequestId = this.request.requestId;
+                let currentCustomer = this.request.customer.customerId;
+                let currentVendor = this.request.vendor.vendorId;
+
+              }
+            });
+            
+            
+          }
+        })
+      }
+    });
   }
 }
